931e41d496aa0739232e62c69bbaf02f5e6978f9
itsmenikoru <bualjefferson64@gmail.com>
2026-02-10 02:55:37 -1000

    1 problem solved

---
+++ 2026/Feb/10/getNthHighestSalary.sql
@@ -0,0 +1,14 @@
+-- Write a solution to find the nth highest distinct salary from the Employee table. 
+-- If there are less than n distinct salaries, return null.
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    set n = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+    select distinct salary 
+    from employee
+    order by salary desc
+    limit 1 offset n
+  );
+END
